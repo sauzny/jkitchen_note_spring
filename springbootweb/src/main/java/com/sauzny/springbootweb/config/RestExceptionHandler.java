f6c0e85e9351ebe9d38f02a4807f514cd56584a7
xinxin <xinxin_949421@126.com>
2018-04-10 10:47:38 +0800

    by: xinxin update

---
+++ springbootweb/src/main/java/com/sauzny/springbootweb/config/RestExceptionHandler.java
@@ -0,0 +1,83 @@
+package com.sauzny.springbootweb.config;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.google.common.collect.Lists;
+import com.sauzny.springbootweb.SbwConstant;
+import com.sauzny.springbootweb.controller.vo.RestFulResult;
+
+import lombok.extern.slf4j.Slf4j;
+
+@ControllerAdvice
+@Slf4j
+public class RestExceptionHandler {
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    private RestFulResult runtimeExceptionHandler(
+            HttpServletRequest request, Exception e) {
+        
+        String ip = request.getRemoteAddr();
+        String url = request.getRequestURI();
+        String methodType = request.getMethod();
+        
+        // 组织参数
+        List<Object> args = Lists.newArrayList("runtime error");
+        args.add(request.getParameterMap());
+        
+        try {
+            String requestStr = IOUtils.toString(request.getInputStream(), StandardCharsets.UTF_8);
+            args.add(requestStr);
+        } catch (IOException e1) {
+        }
+        
+        LogRecord logRecord = new LogRecord(ip, url, methodType, null, args, e.getMessage(), null);
+        
+        log.error(logRecord.toJson());
+        //e.printStackTrace();
+        return RestFulResult.failure();
+    }
+
+    
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    private RestFulResult dataIntegrityViolationExceptionHandler(
+            HttpServletRequest request, DataIntegrityViolationException e) {
+
+        String ip = request.getRemoteAddr();
+        String url = request.getRequestURI();
+        String methodType = request.getMethod();
+        
+        // 组织参数
+        List<Object> args = Lists.newArrayList("DataIntegrityViolationException wtite db error");
+        args.add(request.getParameterMap());
+        
+        try {
+            String requestStr = IOUtils.toString(request.getInputStream(), StandardCharsets.UTF_8);
+            args.add(requestStr);
+        } catch (IOException e1) {
+        }
+        
+        LogRecord logRecord = new LogRecord(ip, url, methodType, null, args, e.getMessage(), null);
+        
+        log.error(logRecord.toJson());
+        
+        return RestFulResult.failure(SbwConstant.FailureEnum.DB_DATA_ILLEGAL);
+    }
+
+}
+
