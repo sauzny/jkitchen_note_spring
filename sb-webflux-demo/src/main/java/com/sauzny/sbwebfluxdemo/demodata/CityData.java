5189a3a9eeed9b7981f2d8ab81ea415913f5eba1
xinxin <xinxin_949421@yahoo.com>
2018-08-30 10:09:37 +0800

    by: xinxin 使用faker伪造数据

--- sb-webflux-demo/src/main/java/com/sauzny/sbwebfluxdemo/demodata/CityData.java
+++ sb-webflux-demo/src/main/java/com/sauzny/sbwebfluxdemo/demodata/CityData.java
@@ -1,7 +1,9 @@
 package com.sauzny.sbwebfluxdemo.demodata;
 
+import java.util.Locale;
 import java.util.Map;
 
+import com.github.javafaker.Faker;
 import com.google.common.collect.Maps;
 import com.sauzny.sbwebfluxdemo.entity.City;
 
@@ -12,21 +14,16 @@ public final class CityData {
 	
 	private CityData() {}
 	
-	public static  Map<Long, City> data = Maps.newHashMap();
+	public static Map<Long, City> data = Maps.newHashMap();
+	
+	public static Faker faker = new Faker(new Locale("zh","CN"));
 	
 	static {
-		data.put(1001L, new City(1001L, "北京市"));
-		data.put(1002L, new City(1002L, "天津市"));
-		data.put(1003L, new City(1003L, "辽宁省沈阳市"));
-		data.put(1004L, new City(1004L, "吉林省长春市"));
-		data.put(1005L, new City(1005L, "黑龙江省哈尔滨市"));
-		data.put(1006L, new City(1006L, "上海市"));
-		data.put(1007L, new City(1007L, "江苏省南京市"));
-		data.put(1008L, new City(1008L, "湖北省武汉市"));
-		data.put(1009L, new City(1009L, "广东省广州市"));
-		data.put(1010L, new City(1010L, "重庆市"));
-		data.put(1011L, new City(1011L, "四川省成都市"));
-		data.put(1012L, new City(1012L, "陕西省西安市"));
+		
+		for(int i=0; i<20; i++) {
+			long id = faker.number().numberBetween(1000, 99999);
+			data.put(id, new City(id, faker.address().city()));
+		}
 		
 		log.info("city测试数据加载完毕");
 	}
