7d45e418938745934b0c0194608e1117422a230a
xinxin <xinxin_949421@yahoo.com>
2018-10-31 17:15:09 +0800

    by: xinxin 数字和字符串的处理还是用lang3吧

---
+++ sb-utils-demo/src/main/java/com/sauzny/sbutilsdemo/other/SpringNumber.java
@@ -0,0 +1,15 @@
+package com.sauzny.sbutilsdemo.other;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringNumber {
+
+	public void demo() {
+
+		// 还是用 lang3 吧 
+		
+		// 数字的格式化 使用 DecimalFormat 吧 
+		
+	}
+}
---
+++ sb-utils-demo/src/main/java/com/sauzny/sbutilsdemo/other/SpringString.java
@@ -0,0 +1,11 @@
+package com.sauzny.sbutilsdemo.other;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringString {
+
+	public void demo() {
+		// 还是用 lang3 吧 
+	}
+}
