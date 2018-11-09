3ef67846b472f1f4c595aa058f517e2984744c27
xinxin <xinxin_949421@yahoo.com>
2018-11-09 18:12:54 +0800

    by: xinxin add base code

---
+++ sb-security-demo/src/main/java/com/sauzny/sbsecuritydemo/security/SecurityUser.java
@@ -0,0 +1,59 @@
+package com.sauzny.sbsecuritydemo.security;
+
+import com.sauzny.sbsecuritydemo.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/***************************************************************************
+ *
+ * ███████╗ █████╗ ██╗   ██╗███████╗███╗   ██╗██╗   ██╗
+ * ██╔════╝██╔══██╗██║   ██║╚══███╔╝████╗  ██║╚██╗ ██╔╝
+ * ███████╗███████║██║   ██║  ███╔╝ ██╔██╗ ██║ ╚████╔╝ 
+ * ╚════██║██╔══██║██║   ██║ ███╔╝  ██║╚██╗██║  ╚██╔╝  
+ * ███████║██║  ██║╚██████╔╝███████╗██║ ╚████║   ██║   
+ * ╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝   ╚═╝   
+ *
+ * @时间: 2018/11/9 - 17:36
+ *
+ * @描述: TODO
+ *
+ ***************************************************************************/
+public class SecurityUser extends User implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
