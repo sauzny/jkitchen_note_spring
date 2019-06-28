8a410fe682ceabe602c0de8ee25e399c4da64b40
xinxin <xinxin_949421@yahoo.com>
2019-06-28 17:44:36 +0800

    spring boot admin 增加 Security，修改服务端和客户端

--- sb-admin-demo/src/main/java/com/sauzny/sbadmindemo/SpringAdminApp.java
+++ sb-admin-demo/src/main/java/com/sauzny/sbadmindemo/SpringAdminApp.java
@@ -1,10 +1,14 @@
 package com.sauzny.sbadmindemo;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 @Configuration
 @EnableAutoConfiguration
@@ -14,5 +18,29 @@ public class SpringAdminApp {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAdminApp.class, args);
 	}
-	
+
+	@Configuration
+	public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+		private final String adminContextPath;
+
+		public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+			this.adminContextPath = adminServerProperties.getContextPath();
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+			successHandler.setTargetUrlParameter("redirectTo");
+
+			http.authorizeRequests()
+					.antMatchers(adminContextPath + "/assets/**").permitAll()
+					.antMatchers(adminContextPath + "/login").permitAll()
+					.anyRequest().authenticated()
+					.and()
+					.formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+					.logout().logoutUrl(adminContextPath + "/logout").and()
+					.httpBasic().and()
+					.csrf().disable();
+		}
+	}
 }
