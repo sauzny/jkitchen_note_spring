9a1168d18ff0b02cac8f05d3ae1d8a3490b133d9
liujinxin <xinxin_949421@yahoo.com>
2019-11-30 16:28:33 +0800

    shardingsphere init

---
+++ sb-sharding-demo/README.md
@@ -0,0 +1,5 @@
+# shardingsphere
+
+官方地址
+
+https://shardingsphere.apache.org/document/current/cn/overview/
--- sb-utils-demo/src/main/java/com/sauzny/sbutilsdemo/SpringUtilsApp.java
+++ sb-utils-demo/src/main/java/com/sauzny/sbutilsdemo/SpringUtilsApp.java
@@ -6,13 +6,15 @@ import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class SpringUtilsApp {
-	
-	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(SpringUtilsApp.class);
-		// 打印PID
-		// 配置文件中可以设置文件打印路径，文件默认位置是项目当前目录application.pid
-		// 执行测试用例的话，是不执行此处代码的
-		app.addListeners(new ApplicationPidFileWriter());
-	    app.run(args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(SpringUtilsApp.class);
+        // 打印PID
+        // 配置文件中可以设置文件打印路径，文件默认位置是项目当前目录application.pid
+        // 执行测试用例的话，是不执行此处代码的
+        app.addListeners(new ApplicationPidFileWriter());
+        app.run(args);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> System.err.println("启动另一个进程，5秒之后把我再启动起来")));
+    }
 }
